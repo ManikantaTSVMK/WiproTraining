21acb174cb5bf23ea77e861b993c8061fa21043f
ManikantaTSVMK <88418685+ManikantaTSVMK@users.noreply.github.com>
2025-07-28 20:52:05 +0530

    Update ClassAndObject.java

--- 28-07-2025_Class Work/ClassAndObject.java
+++ 28-07-2025_Class Work/ClassAndObject.java
@@ -1,6 +1,7 @@
 package com.wipro;
 
-
+import java.io.File;
+import java.util.Scanner;
 
 // class is a blue print or a template that contains data member (attributes) or member functions
 class Employee
@@ -14,7 +15,7 @@ class Employee
 	// this is a special type of method where class name and constructor name are same to initiaze the
 	// memory of class instances or non static variables
 	// Stored in Method Area
-	//Void does not return anything
+	//Void does not return anything .. 
 	// No args constructor
 //	Employee()
 //	{
@@ -22,7 +23,7 @@ class Employee
 //		
 //	}
 	//Args Based constructor
-	 Employee(int empId , String name , int age , String designation)
+	Employee(int empId , String name , int age , String designation)
 	{
 		 // this.empId is used to refer the class or instance variable
 		 // this resolves the ambiguity between the instance variable and local variables if names are same
@@ -39,20 +40,25 @@ class Employee
 	}
 	
 	//this is  a member function or a method of a class - functionality
-	void Input()
+	public static void Display(String organization)
 	{
 		
-		System.out.println("Input method called");
+		System.out.println("You are from :" + organization);
+		
+	}
+	
+	//this is  a member function or a method of a class - functionality
+	public static int Input(int a , int b)
+	{
+		int result = a+b;
+		
+		//System.out.println("The result is :" + result);
+		Display("Wipro");
+		return result;
 		
 	}
 
-	//this is  a member function or a method of a class - functionality
-	void Display()
-	{
-		
-		System.out.println("Output method called");
-		
-	}
+
 
 
 }
@@ -62,11 +68,36 @@ class Employee
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			
-		Employee employee = new Employee(102 , "Jatin" ,40 , "BusinessHead");
-		employee.Input();
-		employee.Display();
+		
+		//Scanner is used to take input from the console  wether it's from keyboard that is
+		//System.in defines or tell to the scanner where to read from and files then it is new File("bac.txt")
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your id :");
+		
+		int id = sc.nextInt();
+		System.out.println("Enter your name :");
+		
+		String name = sc.next();
+		System.out.println("Enter your age :");
+		
+		int age = sc.nextInt();
+		System.out.println("Enter your designation :");
+		
+		String designation = sc.next();
+		
+		Employee employee = new Employee(id , name ,age, designation);
+		System.out.println(employee.Input(23,45));
+		employee.Display("Wipro");
 
 	}
 
 }
+
+ 
+ /*
+  * Constructor rules are given below:
+  * 1. class name as same as constructor name
+  * 2. No return type
+  * 3. It can be parameterized or overloaded and has an access specifier
+  * */
+  
